c163d185056ceec559102878459320a247ed0c1f
AJ Almaguer <ajalmaguer@gmail.com>
2016-04-25 11:11:29 -0700

    fixing js

--- main.js
+++ main.js
@@ -1,77 +1,80 @@
-<script type="text/javascript">
-  if ( !window.requestAnimationFrame ) {
+
+if ( !window.requestAnimationFrame ) {
 
     window.requestAnimationFrame = ( function() {
+
         return window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame ||
         window.oRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
         function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+
             window.setTimeout( callback, 1000 / 60 );
+
         };
+
     } )();
-  }
+
+}
 
 
 
-  var ball;
-  var w;
-  var h;
+var ball;
+var w;
+var h;
 
-  function init()
-  {
-      ball = document.getElementById("ball");
-     w = window.innerWidth;
-       h = window.innerHeight;
+function init()
+{
+    ball = document.getElementById("ball");
+   w = window.innerWidth;
+     h = window.innerHeight;
 
-    ball.style.left = (w/2)-50+"px";
-    ball.style.top = (h/2)-50+"px";
-    ball.velocity = {x:0,y:0}
-    ball.position = {x:0,y:0}
+  ball.style.left = (w/2)-50+"px";
+  ball.style.top = (h/2)-50+"px";
+  ball.velocity = {x:0,y:0}
+  ball.position = {x:0,y:0}
 
-      if (window.DeviceOrientationEvent) {
+    if (window.DeviceOrientationEvent) {
 
-      window.addEventListener("deviceorientation", function(event)
+    window.addEventListener("deviceorientation", function(event)
+    {
+      ball.velocity.y = Math.round(event.beta);
+      ball.velocity.x = Math.round(event.gamma);
+        }
+                               )
+    };
+
+    update();
+}
+
+function update()
+{
+        ball.position.x += ball.velocity.x;
+        ball.position.y += ball.velocity.y;
+
+        if(ball.position.x > (w-100) && ball.velocity.x > 0)
       {
-        ball.velocity.y = Math.round(event.beta);
-        ball.velocity.x = Math.round(event.gamma);
-          }
-                                 )
-      };
+         ball.position.x = w-100;
+      }
 
-      update();
-  }
+      if(ball.position.x < 0 && ball.velocity.x < 0)
+      {
+        ball.position.x = 0;
+      }
 
-  function update()
-  {
-          ball.position.x += ball.velocity.x;
-          ball.position.y += ball.velocity.y;
+      if(ball.position.y > (h-100) && ball.velocity.y > 0)
+      {
+         ball.position.y = h-100;
+      }
 
-          if(ball.position.x > (w-100) && ball.velocity.x > 0)
-        {
-           ball.position.x = w-100;
-        }
+      if(ball.position.y < 0 && ball.velocity.y < 0)
+      {
+         ball.position.y = 0;
+      }
 
-        if(ball.position.x < 0 && ball.velocity.x < 0)
-        {
-          ball.position.x = 0;
-        }
+    ball.style.top = ball.position.y + "px"
+        ball.style.left = ball.position.x + "px"
 
-        if(ball.position.y > (h-100) && ball.velocity.y > 0)
-        {
-           ball.position.y = h-100;
-        }
+    requestAnimationFrame( update );//KEEP ANIMATING
+}
 
-        if(ball.position.y < 0 && ball.velocity.y < 0)
-        {
-           ball.position.y = 0;
-        }
-
-      ball.style.top = ball.position.y + "px"
-          ball.style.left = ball.position.x + "px"
-
-      requestAnimationFrame( update );//KEEP ANIMATING
-  }
-
-</script>
-</html>
